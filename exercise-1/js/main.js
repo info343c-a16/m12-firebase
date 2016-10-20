4af62dc5c25fdf7a2ebbde8dd64bc11ca7796414
mkfreeman <michael.k.freeman@gmail.com>
2016-10-19 17:19:19 -0700

    Removed exercise 1 answers

--- exercise-1/js/main.js
+++ exercise-1/js/main.js
@@ -1,69 +1,40 @@
 // Main.js
 $(function() {
-    // Initialize Firebase using the configuration of your project
-    var config = {
-        apiKey: "AIzaSyCp0ErFpwVFcKeFI5vDjjFFKV0GELFDPTE",
-        authDomain: "test-project-578fc.firebaseapp.com",
-        databaseURL: "https://test-project-578fc.firebaseio.com",
-        storageBucket: "test-project-578fc.appspot.com",
-        messagingSenderId: "919471327541"
-    };
-    firebase.initializeApp(config);
+    // Setup: Initialize Firebase using the configuration of your project
 
-    // Create new database reference 'todos'
-    var todos = firebase.database().ref('todos');
 
+    // Reading Data: Create new database reference 'todos'
+
+
+    // Reading Data:
     // Set listener: on change, empty the todo list, and iterate through to make a new list
-    todos.on('value', function(snapshot) {
-        $('#todo-list').empty();
 
-        // Iterate through elements
-        var items = snapshot.val();
-        if (items !== null) {
-            Object.keys(items).forEach(function(key) {
-                renderTodo(key, items[key]);
-            });
-        }
 
-    });
 
-    // Function to make todos
+
+    // Rendering Data: Function to make todos
     var renderTodo = function(id, content) {
+
         // Create new todo <div> with classes 'todo', and the priority of the item
-        var newTodo = $('<div>').attr('class', 'todo ' + content.priority);
+
 
         // Create an <h5> element, set it's text to the description, and class as the status
-        var text = $('<h5>').text(content.description).attr('class', content.status);
-        newTodo.append(text);
 
-        // Complete icon with click event
-        var completeIcon = $('<i>').attr('class', "fa fa-check fa-2x " + content.status);
-        completeIcon.on('click', function() {
+        // Update Data: create a check icon with click event
             // Flip the status on click
-            var status = content.status == 'complete' ? 'incomplete' : 'complete';
+            // Set the child values of the item
 
-            // Set the child values of this item
-            todos.child(id).set({
-                description: content.description,
-                priority: content.priority,
-                status: status
-            });
-        });
 
-        // Delete icon: on click, remove the reference
-        var deleteIcon = $('<i>').attr('class', "fa fa-times fa-2x");
-        deleteIcon.on('click', function() {
-            todos.child(id).remove();
-        });
+        // Deleting data: Delete icon: on click, remove the reference
 
-        // Append the icons to the newTodo item
-        newTodo.append(completeIcon).append(deleteIcon);
+        // Update/Delete data: append the icons to the newTodo item
 
-        // Append to page
-        $('#todo-list').append(newTodo);
+
+        // Append newTodo item to item with id #todo-list
+
     };
 
-    // Form submission
+    // Reading Data: Form submission
     $('form').on('submit', function(event) {
         event.preventDefault();
 
@@ -71,12 +42,8 @@ $(function() {
         var priority = $(this).find('input:checked')[0].id;
         var text = $(this).find('input').val();
 
-        // Push new item into todos
-        todos.push({
-            description: text,
-            priority: priority,
-            status: 'incomplete'
-        });
+        // Reading Data: Push new item into `todos` reference
+
 
         // Reset the form
         this.reset();
